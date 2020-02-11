5e99f0ed9751990d189c412f882ce2e53054a8e5
wangbin <13331036973@163.com>
2020-02-11 15:26:26 +0800

    20200211

--- src/JsonHyper.js
+++ src/JsonHyper.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var xiaoming = {
+var XiaoMing = {
     name: '小明',
     age: 14,
     gender: true,
@@ -9,9 +9,9 @@ var xiaoming = {
     'middle - school': '\"W3C\" Middle School',
     skills: ['JavaScript', 'Java', 'Python', 'Lisp']
 };
-var s = JSON.stringify(xiaoming, null, ' ');
+var s = JSON.stringify(XiaoMing, null, ' ');
 console.log(s);
-var s2 = JSON.stringify(xiaoming, ['name', 'skills'], '  ');
+var s2 = JSON.stringify(XiaoMing, ['name', 'skills'], '  ');
 console.log(s2);
 
 var qtt = {}; //定义消息（可以为字符串、对象等）
@@ -59,3 +59,50 @@ person = JSON.parse(person); //将字符串转换为json对象
 person.data.push(params); //将传来的对象push进数组对象中
 person.total = person.data.length; //定义一下总条数，为以后的分页打基础
 console.log(person.data);
+
+//json字符串转对象
+var str = '{"persons":[{"id":"1","name":"山姆","age":"23"},{"id":"2","name":"比尔","age":"23"}]}';
+var strToObj = JSON.parse(str);
+console.log(strToObj);
+
+//json对象转字符串
+var obj = {
+    "persons": [{
+        "id": "1",
+        "name": "山姆",
+        "age": "23"
+    }, {
+        "id": "2",
+        "name": "比尔",
+        "age": "23"
+    }]
+} //json对象
+var objToStr = JSON.stringify(obj);
+var objToStr = JSON.stringify(obj, null, ' ');
+console.log(objToStr);
+
+
+//json对象应用
+var person = {
+    "id": "1",
+    "name": "山姆",
+    "age": "23"
+}
+var persons = {
+    "persons": [{
+        "id": "1",
+        "name": "山姆",
+        "age": "23"
+    }, {
+        "id": "2",
+        "name": "比尔",
+        "age": "23"
+    }]
+}
+//查询整个 json 对象的方法
+console.log(persons)
+console.log(persons['persons'])
+console.log(persons['persons'][0])
+console.log(persons['persons'][0]['name'])
+//遍历 json 对象的 key
+console.log(Object.keys(persons))
--- src/test.js
+++ src/test.js
@@ -1,18 +1,79 @@
 //数据体
-var DataJson = '{"id":"1","name":"王彬","age":"23"}';
-//Json 对象
-var persons = {}
-//预录入数据
+var DataJson = '{"persons":[{"id":"1","name":"山姆","age":"23"},{"id":"2","name":"比尔","age":"23"}]}';
+
 var person = {
-    "id": 2,
-    "name": "白眉鹰王",
-    "age": 44
+    "id": "1",
+    "name": "山姆",
+    "age": "23"
 }
-//将二进制的数据转换为字符串
-//var persons = DataJson.toString();
-//将字符串转换为json对象
-persons = JSON.parse(DataJson);
-persons.push(person); //将传来的对象push进数组对象中
-//定义一下总条数，为以后的分页打基础
-//person.total = person.data.length; 
-console.log(persons);
+var persons = {
+    "persons": [{
+        "id": "1",
+        "name": "山姆",
+        "age": "23"
+    }, {
+        "id": "2",
+        "name": "比尔",
+        "age": "23"
+    }]
+}
+
+//获取JSON中的值,直接获取或遍历
+
+//基本格式
+var person = {
+    "name": "张八",
+    "age": 23,
+    "gender": true
+}
+//遍历
+for (var key in person) {
+    //获取键和值 key为字符串格式
+    console.log(key + ":" + person[key]);
+}
+//数组格式
+var ps = [{
+    "name": "张三",
+    "age": 23,
+    "gender": true
+}, {
+    "name": "李四",
+    "age": 24,
+    "gender": true
+}, {
+    "name": "王五",
+    "age": 25,
+    "gender": false
+}]
+//遍历
+for (var i = 0; i < ps.length; i++) {
+    var p = ps[i];
+    for (var key in p) {
+        console.log(key + ":" + p[key]);
+    }
+}
+//嵌套格式
+var persons = {
+    "persons": [{
+            "name": "张三",
+            "age": 23,
+            "gender": true
+        },
+        {
+            "name": "李四",
+            "age": 24,
+            "gender": true
+        },
+        {
+            "name": "王五",
+            "age": 25,
+            "gender": false
+        }
+    ]
+};
+//获取王五的值
+var name = persons.persons[2].name
+console.log('name' + "=" + name);
+
+persons.persons[3] = person
+console.log(persons.persons);
